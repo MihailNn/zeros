a7e3afac6066413e7c1b634bae493124aed69b7a
MihailNn <mihailnaryshkin@gmail.com>
2018-03-04 11:34:29 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
+  let zerosCount = Math.floor(number/5);
+  for(let n = 2, i = Math.pow(5, n); i <= number; n++, i = Math.pow(5, n)) {
+    zerosCount += Math.floor(number / i);
+  }
+  return zerosCount;
   // your implementation
 }
